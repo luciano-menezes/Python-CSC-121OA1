c1fe9da6ec0e3f4dbf0ef0b27841d804997675e7
Luciano Menezes <101853016+luciano-menezes@users.noreply.github.com>
2024-01-27 12:53:32 -0500

    Add files via upload

---
+++ simple_messages.py
@@ -0,0 +1,5 @@
+simple_message_1 = "This the first simple message!"
+print(simple_message_1)
+
+simple_message_1 = "The variable name is the same, but the value is different!"
+print(simple_message_1)
