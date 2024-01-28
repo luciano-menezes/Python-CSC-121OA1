838c87a0627a4aca25b42d48d31e00d892af6f13
Luciano Menezes <101853016+luciano-menezes@users.noreply.github.com>
2024-01-28 11:51:51 -0500

    Add files via upload

---
+++ famous_quote.py
@@ -0,0 +1,4 @@
+famous_person = "Jimi Hendrix"
+message = 'once said, "When the power of love overcomes the love of power, the world will know peace."'
+famous_quote = f"{famous_person} {message}"
+print(famous_quote)
