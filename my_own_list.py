56b308b47f1b7fb245564f0197f89f742c9c63c0
Luciano Menezes <101853016+luciano-menezes@users.noreply.github.com>
2024-01-30 20:56:47 -0500

    Add files via upload

---
+++ my_own_list.py
@@ -0,0 +1,14 @@
+cars = ['Jeep', 'Porsche', 'Lamborghini', 'Ferrari']
+
+first_message = f"My wife's favorite car is a {cars[0].title()}!"
+print(first_message)
+
+second_message = f"I always thought that the {cars[3].title()} was an awesome car!"
+print(second_message)
+
+#I'm sure there's a better way to turn the item from singular to plural, but I did this way just for now.
+third_message = f"When I was living in Miami, I saw a lot of {cars[2].title()}s!" 
+print(third_message)
+
+
+
