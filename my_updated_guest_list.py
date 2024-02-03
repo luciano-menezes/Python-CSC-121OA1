304f5afdfd830ccb322225c17342913334ae34fc
Luciano Menezes <101853016+luciano-menezes@users.noreply.github.com>
2024-02-02 22:23:43 -0500

    Add files via upload

---
+++ my_updated_guest_list.py
@@ -0,0 +1,21 @@
+guest_list = ['Ayrton Senna', 'Pele', 'David Gilmour']
+print(f"Hi {guest_list[0]}, I'd like to invite you to my next dinner event.")
+print(f"\nHi {guest_list[1]}, I'd like to invite you to my next dinner event.")
+print(f"\nHi {guest_list[2]}, I'd like to invite you to my next dinner event.")
+print(f"\n Hello {guest_list[0]}, {guest_list[1]} and {guest_list[2]}, I'd like to inform that I found a bigger table and we'll be having more guests joining us.\n")
+
+guest_list.insert(0, 'Jerry Seinfeld')
+guest_list.insert(2, 'Scooby-Doo')
+guest_list.append('Shaggy')
+guest_list.sort()
+print(guest_list)
+
+print(f"\nHi {guest_list[0]}, I'd like to invite you to my next dinner event.")
+print(f"\nHi {guest_list[1]}, I'd like to invite you to my next dinner event.")
+print(f"\nHi {guest_list[2]}, I'd like to invite you to my next dinner event.")
+print(f"\nHi {guest_list[3]}, I'd like to invite you to my next dinner event.")
+print(f"\nHi {guest_list[4]}, I'd like to invite you to my next dinner event.")
+print(f"\nHi {guest_list[5]}, I'd like to invite you to my next dinner event.")
+
+
+
