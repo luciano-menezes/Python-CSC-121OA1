65915d0ee2ef9769e240f89f5fdd8134d25aac92
Luciano Menezes <101853016+luciano-menezes@users.noreply.github.com>
2024-02-02 21:53:28 -0500

    Delete my_guest_list.py

--- my_guest_list.py
+++
@@ -1,4 +0,0 @@
-guest_list = ['Ayrton Senna', 'Pele', 'David Gilmour']
-print(f"Hi {guest_list[0].title()}, I'd like to invite you to my next dinner event.")
-print(f"\nHi {guest_list[1].title()}, I'd like to invite you to my next dinner event.")
-print(f"\nHi {guest_list[2].title()}, I'd like to invite you to my next dinner event.")
