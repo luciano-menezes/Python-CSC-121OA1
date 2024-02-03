a2770e68591635a0d5ef8012d5bc8cd6d01af54b
Luciano Menezes <101853016+luciano-menezes@users.noreply.github.com>
2024-02-02 21:53:51 -0500

    Add files via upload

---
+++ my_guest_list.py
@@ -0,0 +1,4 @@
+guest_list = ['Ayrton Senna', 'Pele', 'David Gilmour']
+print(f"Hi {guest_list[0]}, I'd like to invite you to my next dinner event.")
+print(f"\nHi {guest_list[1]}, I'd like to invite you to my next dinner event.")
+print(f"\nHi {guest_list[2]}, I'd like to invite you to my next dinner event.")
