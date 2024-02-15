470d581ee0571455f9a38b0e814dbe1de254cc7f
Luciano Menezes <101853016+luciano-menezes@users.noreply.github.com>
2024-02-15 09:03:46 -0500

    Add files via upload

---
+++ alien_colors.py
@@ -0,0 +1,6 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+  print("You earned 5 points for shooting down an alien!")
+if alien_color == 'yellow':
+  print("You earned 5 points for shooting down an alien")
