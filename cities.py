d59e3601a8d190d4a32c10a3a75e3c461ff5571a
Luciano Menezes <101853016+luciano-menezes@users.noreply.github.com>
2024-03-04 15:33:06 -0500

    Add files via upload

---
+++ cities.py
@@ -0,0 +1,7 @@
+def describe_city(city, country='Brazil'):
+  """Displays information about the city."""
+  print(f"\n{city} is in {country}!")
+
+describe_city(city='Salvador', country="Brazil")
+describe_city(city='Barcelona', country="Spain")
+describe_city(city='Buenos Aires', country="Argentina")
